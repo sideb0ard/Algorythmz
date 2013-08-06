fa4c777b773a81f03b5d1f0da8fb486c48077c9e
Thorsten Sideb0ard <thorsten.sideb0ard@gmail.com>
2013-08-05 23:23:52 -0700

    Finished a version of the scala code

--- Sortrrr.scala
+++ Sortrrr.scala
@@ -1,6 +1,37 @@
+import scala.collection.mutable.ListBuffer
 object mergeSortProg {
 
-  //def merge(array1
+  def merge(l1: List[Int], l2: List[Int]): List[Int] = {
+    println("Starting Merge")
+    val sortedList = ListBuffer.empty[Int]
+    val l1_buffer = l1.to[ListBuffer]
+    val l2_buffer = l2.to[ListBuffer]
+
+    while(l1_buffer.length > 0 || l2_buffer.length > 0) {
+      if(l1_buffer.length > 0 && l2_buffer.length > 0) {
+        if(l1_buffer.head <= l2_buffer.head) {
+          println("l1.head smaller than l2.head -> appending to sortedList")
+          sortedList += l1_buffer.head
+          l1_buffer remove 0
+        } else {
+          println("l2.head smaller - appending to sortedList")
+          sortedList += l2_buffer.head
+          l2_buffer remove 0
+        }
+      } else if(l1_buffer.length > 0) {
+        println("no more l2 - putting rest of l1 to sortedList")
+        sortedList += l1_buffer.head
+        l1_buffer remove 0
+      } else {
+        println("no more l1 - putting rest of l2 to sortedList")
+        sortedList += l2_buffer.head
+        l2_buffer remove 0
+      }
+    }
+    println("About to return == sortedList = " + sortedList.length)
+    return sortedList.toList
+  }
+
 
   def mergeSort(intz: List[Int]): List[Int] = {
 
@@ -12,25 +43,16 @@ object mergeSortProg {
     val halfSize = intz.length / 2
     println("HALF LENGTH is " + halfSize)
 
-    val splitTuple = intz splitAt halfSize 
-    var listOne: List[Int] = splitTuple._1
-    var listTwo: List[Int] = splitTuple._2
+    val (listOne,listTwo) = intz splitAt halfSize
 
     println("ListOne iz " + listOne.mkString(" "))
     println("ListTwo iz " + listTwo.mkString(" "))
 
-    listOne = mergeSort(listOne)
-    listTwo = mergeSort(listTwo)
+    val l1 = mergeSort(listOne)
+    val l2 = mergeSort(listTwo)
 
-    //if (intzList.length <= 1)
-    //  return intzList
-    //else
-    //  mergeSort(intzList)
-      // return intzList
-
-    val dummyList: List[Int] = List(43, 53, 12)
-    dummyList
-
+    val sortedList: List[Int] = merge(l1,l2)
+    return sortedList
   }
 }
 
